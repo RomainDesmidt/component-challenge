e7e35e24918e2c3b84058b25eecf90468536cd5f
Romain DESMIDT <rdesmidt47@gmail.com>
2023-05-16 12:10:40 +0200

    added block component content, call and import in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { BlockComponent } from "./block/block.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BlockComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
